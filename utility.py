d83f9d12e5e9730202b2f900621e101d3e543443
Colin Seifert <colinseifert@uvic.ca>
2024-07-15 14:52:33 -0700

    updated with webdriverwait conditions,  play-until-failure, or a number of moves

--- utility.py
+++ utility.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support  import expected_conditions as EC
 
 UP = Keys.ARROW_UP
 DOWN = Keys.ARROW_DOWN
@@ -20,13 +22,29 @@ class utility:
         self.gameGrid = numpy.zeros((4,4))
         return
 
+    def runGame(self,numMoves):
+        self.driver.get("https://play2048.co/")
+        selector = "//*[contains(@class, 'tile tile-')]"
+        waitCond = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, selector))) #stored as a variable for debugging? 
+        stopCondition = "body > div.container.upper > div.game-container > div.game-message.game-over"
+        while(len(self.driver.find_elements(By.CSS_SELECTOR, stopCondition)) == 0 and numMoves != 0):
+            waitCond = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, selector)))
+            move = self.generateRandomMove()
+            self.sendMove(move)
+            numMoves -= 1
+        finalState = self.getGameState()
+        self.printGameGrid()
+        self.driver.quit()
+        return finalState
+
+
 
     def getGameState(self):
         row = 0
         col = 0
         selector = "//*[contains(@class, 'tile tile-')]"
 
-        time.sleep(0.75) #wait for page to catch up after each move
+        # time.sleep(0.75) #wait for page to catch up after each move
         elements = self.driver.find_elements(By.XPATH, selector)
         #print(elements)
         for element in elements:
@@ -40,7 +58,7 @@ class utility:
 
     def generateRandomMove(self)->str:
         num = random.randint(1,4)
-        print("randomMove:",num)
+        #print("randomMove:",num)
         if(num == 1):
             return UP
         if(num == 2):
@@ -60,17 +78,19 @@ class utility:
         return
     
 
-
 game = utility()
-game.driver.get("https://play2048.co/")
-time.sleep(3)
+game.runGame(-1) #positive value to execute a specific number of moves, set to -1 to play a full game
 
-for __ in range(10):
-    game.getGameState()
-    game.printGameGrid()
-    move = game.generateRandomMove()
-    game.sendMove(move)
-    #time.sleep(0.1) #let page update
+# game = utility()
+# game.driver.get("https://play2048.co/")
+# time.sleep(3)
+
+# for __ in range(10):
+#     game.getGameState()
+#     game.printGameGrid()
+#     move = game.generateRandomMove()
+#     game.sendMove(move)
+#     #time.sleep(0.1) #let page update
 
 
 
